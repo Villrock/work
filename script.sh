399cc69dcbe9986b8be67c387cccc33fcb1980f5
Villrock <myjobmaill13@gmail.com>
2016-04-09 12:39:12 +0300

    Delete script.sh

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-   sudo apt-get install nginx -y
-   sudo chown -R www-data:www-data /usr/share/nginx/html
-   cd /etc/nginx/sites-enabled/
-   sudo perl -i -pe 'print "proxy_pass http://192.168.10.2:9090;" if $. == 34' default
-   sudo service nginx stop
-   sudo service nginx start
